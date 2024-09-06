6c0836ce59aa29f614f0f2310d2338d37ad945b2
Leonid <leonid.spb2015@yandex.ru>
2024-09-06 20:12:21 +0300

    Coding site - 1 page

---
+++ dist/scripts/main.js
@@ -0,0 +1,9 @@
+
+  
+let searchBlock = document.querySelector('.search-block');
+let searchBlockOpen = document.querySelector('.search-block-open');
+
+searchBlock.addEventListener('click', function() {
+  searchBlockOpen.style.display = 'block';
+})
+
