45a15969a9fb8fa41b7251c75b627286fac28e66
Leonid <leonid.spb2015@yandex.ru>
2024-09-07 21:19:47 +0300

    Добавил слайдерs

---
+++ dist/sliders/Featured Items_slider/Owl Carousel/initialize_slider.js
@@ -0,0 +1,22 @@
+
+$(document).ready(function(){
+  $(".slider-one").owlCarousel({
+    items: 5,
+    loop: true,
+    margin: 10,
+    nav: true,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: true
+  });
+
+  $(".slider-two").owlCarousel({
+    items: 5,
+    loop: true,
+    margin: 10,
+    nav: true,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: true
+  });
+});
