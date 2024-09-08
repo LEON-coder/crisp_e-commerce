68a344415c39a95099dc30b2afc8add5f4e87132
Leonid <leonid.spb2015@yandex.ru>
2024-09-08 20:19:54 +0300

    Добавил анимацию появления для всплывашки окна регистрации

--- dist/scripts/sign-in.js
+++ dist/scripts/sign-in.js
@@ -1,14 +1,23 @@
 let signIn = document.querySelector('.header__sign-in');
+let signInForm = document.querySelector('.sign-in-form');
 let popUp = document.querySelector('.sign-in__pop-up');
 let popUpClose = document.querySelector('.pop-up-close');
 
 
+
 signIn.addEventListener('click' , function() {
   popUp.classList.add('active');
+
+  const timerId = setTimeout(() => {
+    signInForm.classList.add('sign-in-form-active');
+  }, 1000);
+  
+
 })
 
 popUpClose.addEventListener('click' , function() {
   popUp.classList.remove('active');
+  signInForm.classList.remove('sign-in-form-active');
 })
 
 
