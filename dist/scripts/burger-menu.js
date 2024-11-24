b168d7b53b566e145804569a705be2dfeb8e89ff
Leonid <leonid.spb2015@yandex.ru>
2024-11-24 20:23:40 +0300

    Делаю мобильное меню

---
+++ dist/scripts/burger-menu.js
@@ -0,0 +1,17 @@
+let BurgerMenuIcon=document.querySelector('.navigation__mobile-menu');
+let navigationMenu=document.querySelector('.navigation');
+let menuStripe=document.querySelector('.mobile-menu-stripe');
+let header=document.querySelector('.header');
+let CardBlock=document.querySelector('.header__cart-block');
+let BlockAutorization=document.querySelector('.header__block-autorization');
+
+
+BurgerMenuIcon.addEventListener('click', function() {
+    navigationMenu.classList.toggle('open_menu');
+    header.classList.toggle('header_open');
+    menuStripe.classList.toggle('navigation__mobile-menu--close');
+  CardBlock.classList.toggle('open');
+  this.classList.toggle('pulse-animation')
+  }
+
+);
